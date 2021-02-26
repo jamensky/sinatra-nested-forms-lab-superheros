8513c2b707572d0d014c360b912366c5cedfc511
Jamal <jamensky1991@gmail.com>
2021-02-26 21:34:22 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,13 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do 
-     binding.pry
-      'hello'#erb :super_hero 
+      erb :super_hero 
     end 
 
-    post '/team' do
+    post '/teams' do
+      binding.pry 
+
+      @team = params
       erb :team
     end 
 
